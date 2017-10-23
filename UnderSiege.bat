ab28d56a0a249b5dccabd7f8d1257ec9e2e3a432
student <student>
2017-10-23 11:57:05 -0400

    Batch file to quickly setup on school pc

---
+++ UnderSiege.bat
@@ -0,0 +1,5 @@
+git config --global http.sslVerify false
+CD C:\Users\ekustudent\Desktop
+git clone https://code.eku.edu/michael_rawlings6/UnderSiege.git
+start /d "C:\Program Files\Unity\Editor" Unity.exe
+start cmd.exe /K "CD C:\Users\ekustudent\Desktop\UnderSiege"
