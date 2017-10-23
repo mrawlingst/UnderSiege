6c5f6a4ff82fccfc632d335257d5383e2df04a6e
Michael Rawlings <michael_rawlings6@mymail.eku.edu>
2017-10-23 12:08:51 -0400

    Update bat file

--- UnderSiege.bat
+++ UnderSiege.bat
@@ -1,5 +1,15 @@
+:: Turn off SSL Verification
 git config --global http.sslVerify false
-CD C:\Users\ekustudent\Desktop
-git clone https://code.eku.edu/michael_rawlings6/UnderSiege.git
+
+:: Change your name and email here
+git config --global user.name "YOUR NAME"
+git config --global user.email "YOUR_EMAIL@mymail.eku.edu"
+
+:: Clone repo
+git clone https://code.eku.edu/michael_rawlings6/UnderSiege.git C:\Users\ekustudent\Desktop\UnderSiege
+
+:: Start unity
 start /d "C:\Program Files\Unity\Editor" Unity.exe
-start cmd.exe /K "CD C:\Users\ekustudent\Desktop\UnderSiege"
+
+:: Start new cmd at repo's folder
+start cmd.exe /k "cd /d C:\Users\ekustudent\Desktop\UnderSiege"
