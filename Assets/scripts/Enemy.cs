92817aac23ee16c1d59c4042dd552552a0b19c21
Michael Rawlings <michael_rawlings6@mymail.eku.edu>
2017-10-25 12:46:15 -0400

    Ability to kill skeelton

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int health = 100;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        health -= 10;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("LEFT COLLIDED");
+    }
+}
