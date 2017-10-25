edb918819e418e50ab7493bb7182f297e8df19e7
Michael Rawlings <michael_rawlings6@mymail.eku.edu>
2017-10-25 09:22:53 -0400

    Update bat to open unity project

--- UnderSiege.bat
+++ UnderSiege.bat
@@ -9,7 +9,7 @@ git config --global user.email "YOUR_EMAIL@mymail.eku.edu"
 git clone https://code.eku.edu/michael_rawlings6/UnderSiege.git C:\Users\ekustudent\Desktop\UnderSiege
 
 :: Start unity
-start /d "C:\Program Files\Unity\Editor" Unity.exe
+start /d "C:\Program Files\Unity\Editor" Unity.exe -projectPath "C:\Users\ekustudent\Desktop\UnderSiege"
 
 :: Start new cmd at repo's folder and on develop branch
 start cmd.exe /k "cd /d C:\Users\ekustudent\Desktop\UnderSiege & git checkout -b develop origin/develop"
