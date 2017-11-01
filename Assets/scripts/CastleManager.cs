3535efe4ab3608892bf25789aa191e8a2b202334
Thomas Bowlin <thomas_bowlin31@mymail.eku.edu>
2017-11-01 14:06:22 -0400

    Start basic castle health system

---
+++ Assets/scripts/CastleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleManager : MonoBehaviour
+{
+    private int castleHealth = 5;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public int getHealth()
+    {
+        return castleHealth;
+    }
+}
