81d0ccbd9cfad5daf94e91b4997fc1edce2e0994
Michael Rawlings <michael_rawlings6@mymail.eku.edu>
2017-11-17 11:36:04 -0500

    Add health indicator - light

--- Assets/scripts/CastleManager.cs
+++ Assets/scripts/CastleManager.cs
@@ -3,16 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class CastleManager : MonoBehaviour
 {
+    public int maxCastleHealth = 20;
     public int CastleHealth = 20;
+
+    public Light light;
+    public Color dyingLight = Color.red;
+
+    private Color healthyLight;
+
+    void Awake()
+    {
+        healthyLight = light.color;
+    }
 
     public void TakeDamage(int damage)
     {
         CastleHealth -= damage;
 
+        var newColor = Color.Lerp(dyingLight, healthyLight, ((float)CastleHealth / (float)maxCastleHealth));
+        light.color = newColor;
+
         if (CastleHealth <= 0)
         {
+            Debug.Log("???????????????");
             SceneManager.LoadScene(1);
-       } 
+        }
     }
 
     void OnTriggerEnter(Collider other)
